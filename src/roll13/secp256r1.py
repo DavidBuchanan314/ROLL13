7c809aca1d01bdbdc9c8b94d56fde88162a827a3
David Buchanan <d@vidbuchanan.co.uk>
2021-07-14 22:35:44 +0100

    Implement AES GCM

--- src/roll13/secp256r1.py
+++ src/roll13/secp256r1.py
@@ -6,19 +6,19 @@
 # Section 2.7.2 - Curve Parameters
 
 # the modulus of the prime field
-p = 0xffffffff_00000001_00000000_00000000_00000000_ffffffff_ffffffff_ffffffff
+p  = 0xffffffff_00000001_00000000_00000000_00000000_ffffffff_ffffffff_ffffffff
 
 # curve parameters
-a = p - 3
-b = 0x5ac635d8_aa3a93e7_b3ebbd55_769886bc_651d06b0_cc53b0f6_3bce3c3e_27d2604b
+a  = p - 3
+b  = 0x5ac635d8_aa3a93e7_b3ebbd55_769886bc_651d06b0_cc53b0f6_3bce3c3e_27d2604b
 
 # the base point
 Gx = 0x6b17d1f2_e12c4247_f8bce6e5_63a440f2_77037d81_2deb33a0_f4a13945_d898c296
 Gy = 0x4fe342e2_fe1a7f9b_8ee7eb4a_7c0f9e16_2bce3357_6b315ece_cbb64068_37bf51f5
 
 # the order of the field
-n = 0xffffffff_00000000_ffffffff_ffffffff_bce6faad_a7179e84_f3b9cac2_fc632551
-h = 1 # cofactor
+n  = 0xffffffff_00000000_ffffffff_ffffffff_bce6faad_a7179e84_f3b9cac2_fc632551
+h  = 1 # cofactor
 
 
 class Point():
@@ -64,9 +64,9 @@ class Point():
 		if P.x == Q.x and P.y == -Q.y:
 			return Infinity.__copy__()
 		if P == Q: # point doubling
-			tmp = ((3 * pow(P.x, 2, p) + a) * pow(2 * P.y, p - 2, p)) % p
+			tmp = ((3 * pow(P.x, 2, p) + a) * pow(2 * P.y, -1, p)) % p
 		else:
-			tmp = ((Q.y - P.y) * pow(Q.x - P.x, p - 2, p)) % p
+			tmp = ((Q.y - P.y) * pow(Q.x - P.x, -1, p)) % p
 		x3 = (pow(tmp, 2, p) - P.x - Q.x) % p
 		y3 = (tmp*(P.x - x3) - P.y) % p
 		return Point(x3, y3)
